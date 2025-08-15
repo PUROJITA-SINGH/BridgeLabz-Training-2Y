81d1dcdc7582b1ab60e878424601562f1452a5c6
PUROJITA-SINGH <purojitasinghixc20@gmail.com>
2025-08-15 13:06:05 +0530

    delete file

--- Collections-Practice/ArrayListExample.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-public class ArrayListExample {
-    public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>();
-        list.add("Apple");
-        list.add("Banana");
-        System.out.println("List: " + list);
-    }
-}
--- Threads-Assignment/SimpleThread.java
+++
@@ -1,9 +0,0 @@
-public class SimpleThread extends Thread {
-    public void run() {
-        System.out.println("Thread is running: " + Thread.currentThread().getName());
-    }
-    public static void main(String[] args) {
-        SimpleThread t1 = new SimpleThread();
-        t1.start();
-    }
-}
