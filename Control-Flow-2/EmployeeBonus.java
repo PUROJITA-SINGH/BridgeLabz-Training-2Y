ad7ef12554c53bfd749aa07ce71b2d643bb2aa28
PUROJITA-SINGH <purojitasinghixc20@gmail.com>
2025-08-31 15:57:26 +0530

    [Purojita]-Add.EmployeeBonus java code and class to Control-Flow-2 directory/folder

---
+++ Control-Flow-2/EmployeeBonus.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class EmployeeBonus {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter salary: ");
+        double salary = scanner.nextDouble();
+        System.out.println("Enter years of service: ");
+        int years = scanner.nextInt();
+        double bonus = (years > 5) ? salary * 0.05 : 0.0;
+        System.out.println("Bonus amount: " + bonus);
+        scanner.close();
+    }
+}
