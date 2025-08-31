a8376297f114d356c3a653e06e98271f143fca47
PUROJITA-SINGH <purojitasinghixc20@gmail.com>
2025-08-31 16:17:39 +0530

    [Purojita]-Add.MultiplesWhile java code and class to Control-Flow-2 directory/folder

---
+++ Control-Flow-2/MultiplesWhile.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MultiplesWhile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a positive integer less than 100: ");
+        int number = scanner.nextInt();
+        if (number > 0 && number < 100) {
+            System.out.println("Multiples of " + number + " below 100:");
+            int counter = 100;
+            while (counter >= 1) {
+                if (counter % number == 0) {
+                    System.out.println(counter);
+                }
+                counter--;
+            }
+        } else {
+            System.out.println("Please enter a positive integer less than 100.");
+        }
+        scanner.close();
+    }
+}
