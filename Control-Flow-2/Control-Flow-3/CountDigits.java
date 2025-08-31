7df682e157538e9e228cbac4468055c13407024c
PUROJITA-SINGH <purojitasinghixc20@gmail.com>
2025-08-31 16:32:04 +0530

    [Purojita]-Add.CountDigits java code and class to Control-Flow-3 directory/folder

---
+++ Control-Flow-2/Control-Flow-3/CountDigits.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+        int count = 0;
+        int temp = Math.abs(number);
+        do {
+            temp /= 10;
+            count++;
+        } while (temp != 0);
+        System.out.println("Number of digits in " + number + ": " + count);
+        scanner.close();
+    }
+}
+
