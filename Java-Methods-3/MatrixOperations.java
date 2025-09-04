62d5fffc27620a7121e9d5ac3eec78e524cc67c7
PUROJITA-SINGH <purojitasinghixc20@gmail.com>
2025-09-04 12:31:24 +0530

    [Purojita]-Add.MatrixOperations Java and class file added to folder Java-Methods-3

---
+++ Java-Methods-3/MatrixOperations.java
@@ -0,0 +1,82 @@
+import java.util.Random;
+
+public class MatrixOperations {
+    public static void main(String[] args) {
+        int rows = 2, cols = 2;
+        int[][] mat1 = createRandomMatrix(rows, cols);
+        int[][] mat2 = createRandomMatrix(rows, cols);
+        
+        System.out.println("Matrix 1:");
+        displayMatrix(mat1);
+        System.out.println("Matrix 2:");
+        displayMatrix(mat2);
+        
+        System.out.println("Addition:");
+        displayMatrix(addMatrices(mat1, mat2));
+        
+        System.out.println("Subtraction:");
+        displayMatrix(subtractMatrices(mat1, mat2));
+        
+        System.out.println("Multiplication:");
+        displayMatrix(multiplyMatrices(mat1, mat2));
+    }
+    
+    public static int[][] createRandomMatrix(int rows, int cols) {
+        Random random = new Random();
+        int[][] matrix = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = random.nextInt(10);
+            }
+        }
+        return matrix;
+    }
+    
+    public static int[][] addMatrices(int[][] mat1, int[][] mat2) {
+        int rows = mat1.length;
+        int cols = mat1[0].length;
+        int[][] result = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[i][j] = mat1[i][j] + mat2[i][j];
+            }
+        }
+        return result;
+    }
+    
+    public static int[][] subtractMatrices(int[][] mat1, int[][] mat2) {
+        int rows = mat1.length;
+        int cols = mat1[0].length;
+        int[][] result = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[i][j] = mat1[i][j] - mat2[i][j];
+            }
+        }
+        return result;
+    }
+    
+    public static int[][] multiplyMatrices(int[][] mat1, int[][] mat2) {
+        int rows1 = mat1.length;
+        int cols1 = mat1[0].length;
+        int cols2 = mat2[0].length;
+        int[][] result = new int[rows1][cols2];
+        for (int i = 0; i < rows1; i++) {
+            for (int j = 0; j < cols2; j++) {
+                for (int k = 0; k < cols1; k++) {
+                    result[i][j] += mat1[i][k] * mat2[k][j];
+                }
+            }
+        }
+        return result;
+    }
+    
+    public static void displayMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+}
