05a61e7782a1edde52fec73bd65af49cc09f9f53
PUROJITA-SINGH <purojitasinghixc20@gmail.com>
2025-11-02 15:55:16 +0530

    [Purojita]-Add.Code file of Package Hierarchy

---
+++ src1/MainApp.java
@@ -0,0 +1,7 @@
+import com.university.department.cse.Course;
+
+public class MainApp {
+    public static void main(String[] args) {
+        new Course().display();
+    }
+}
---
+++ src1/com/university/department/cse/Course.java
@@ -0,0 +1,7 @@
+package com.university.department.cse;
+
+public class Course {
+    public void display() {
+        System.out.println("Course: Data Structures (CSE Department)");
+    }
+}
