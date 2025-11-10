6e921428bdfcdd7f73ad2cb2944bd0d135e988e4
PUROJITA-SINGH <purojitasinghixc20@gmail.com>
2025-11-10 15:18:58 +0530

    [Purojita]-Add.Code files for Collections Assignment

---
+++ Collections/NthElementFromEnd.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class NthElementFromEnd {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>(Arrays.asList("A", "B", "C", "D", "E"));
+        int n = 2;
+
+        String result = findNthFromEnd(list, n);
+        System.out.println("Nth element from end: " + result);
+    }
+
+    static String findNthFromEnd(LinkedList<String> list, int n) {
+        Iterator<String> first = list.iterator();
+        Iterator<String> second = list.iterator();
+
+        for (int i = 0; i < n && first.hasNext(); i++)
+            first.next();
+
+        while (first.hasNext()) {
+            first.next();
+            second.next();
+        }
+
+        return second.next();
+    }
+}
